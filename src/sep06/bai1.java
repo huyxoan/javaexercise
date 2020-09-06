75552164237adddb7ec119ffe85a8ab8afe5f922
Huy Xoan <huyxoan@gmail.com>
2020-09-06 21:45:24 +0700

    Nopbaitap ngay 6/9

---
+++ src/sep06/bai1.java
@@ -0,0 +1,23 @@
+package sep06;
+
+import java.util.Scanner;
+
+/***
+ *  Nhập số nguyên n, tìm tổng các ước số của n
+ */
+public class bai1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap mot so nguyen  : ");
+        int n = sc.nextInt();
+        int s = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                s += i;
+            }
+        }
+
+        System.out.println("Ket qua la " + s);
+    }
+}
