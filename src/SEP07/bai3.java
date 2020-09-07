4763fc13ef0ac4cfa6a018beb9234a80a2d01d51
Huy Xoan <huyxoan@gmail.com>
2020-09-07 22:37:21 +0700

    Nopbaitap ngay 7/9

---
+++ src/SEP07/bai3.java
@@ -0,0 +1,25 @@
+package SEP07;
+
+import java.util.Scanner;
+
+/***
+ * . Viết chương trình nhập vào một số nguyên rồi kiểm tra các chữ số trong sô có phải tăng dần hay không (không được phép chuyển về kiểu String)
+ */
+public class bai3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap vao mot so nguyen : ");
+        int n = sc.nextInt();
+        System.out.println(KT(n));
+    }
+
+    static boolean KT(int n) {
+        while (n > 0) {
+            int p = n % 10;
+            int p1 = (n / 10) % 10;
+            n /= 10;
+            if (p < p1) return false;
+        }
+        return true;
+    }
+}
