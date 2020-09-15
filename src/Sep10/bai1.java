54fa7287870b9a7f12ead59a90363bac4f98139f
Huy Xoan <huyxoan@gmail.com>
2020-09-15 20:47:54 +0700

    bai3 , bai 1 Sep 10 done

--- src/Sep10/bai1.java
+++ src/Sep10/bai1.java
@@ -7,13 +7,25 @@ import java.util.Scanner;
  */
 public class bai1 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in) ;
-        System.out.println("Nhap so nguyen n : ");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhap so nguyen n : ");
         int n = sc.nextInt();
         int[] arr = new int[n];
-        for(int i = 0 ; i <= n ; i++){
-   //         System.out.println("A[ +" i " + ] = " );
-            arr[i] = sc.nextInt() ;
-         }
+        for (int i = 0; i < n; i++) {
+            System.out.print("A[ " + i + " ] = ");
+            arr[i] = sc.nextInt();
+        }
+        if (KT(arr, n)) {
+            System.out.println("Mang co gia tri tang dan ");
+        } else {
+            System.out.println("Mang khong tang dan");
+        }
+    }
+
+    static boolean KT(int b[], int p) {
+        for (int i = 0; i < p - 1; i++) {
+            if (b[i] > b[i + 1]) return false;
+        }
+        return true;
     }
 }
---
+++ src/Sep10/bai3.java
@@ -0,0 +1,27 @@
+package Sep10;
+
+import java.util.Scanner;
+
+/***
+ * Viết chương trình nhập vào một mảng số nguyên,
+ * in ra giá trị lớn nhất có thể khi đem 2 giá trị liền kề nhân với nhau.
+ */
+public class bai3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhap so nguyen n : ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            System.out.print("A[ " + i + " ] = ");
+            arr[i] = sc.nextInt();
+        }
+        int max = 0;
+        for (int i = 0; i < n - 1; i++) {
+                if (arr[i] * arr[i+1] > max) {
+                    max = arr[i] * arr[i+1];
+                }
+        }
+        System.out.println(max);
+    }
+}
